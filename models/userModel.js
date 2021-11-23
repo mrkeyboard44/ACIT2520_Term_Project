126464839bf657afe9bcac34b15b3e4ec360198e
Alyssa <achampion4@my.bcit.ca>
2021-11-23 12:56:21 -0800

    git hub profile works

--- models/userModel.js
+++ models/userModel.js
@@ -3,11 +3,6 @@ const { database } = require("./userDatabase");
 require('dotenv').config()
 const process = require('process');
 
-const getRandomImage = async () => {
-  const response = await fetch(`https://api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_CLIENT_ID}`)
-  const jsonData =  await response.json()
-  return jsonData.urls.regular
-}
 const userModel = {
   findOne: (email) => {
     const user = database.find((user) => user.email === email);
@@ -34,10 +29,8 @@ const userModel = {
         email: null,
         password: null,
         reminders: [],
-        image: "",
+        image: profile._json.avatar_url,
       };
-
-      newUser.image = getRandomImage()
       database.push(newUser)
       const user = database.find((user) => user.id == profile.id);
       return user
